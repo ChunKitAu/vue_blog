fe1726441742c5749993886e1ca98e5ac77db3ab
ChunKitAu <570137511@qq.com>
2020-07-05 15:44:16 +0800

    修改页脚

--- src/components/layout/layout-footer.vue
+++ src/components/layout/layout-footer.vue
@@ -1,50 +1,16 @@
 <template>
     <div id="layout-footer">
-<!--        <div class="footer-main">-->
-<!--            <div class="footer-item" v-if="socials.length">-->
-<!--                <div v-for="item in socials" :key="item.id"><a target="_blank" class="out-link" :href="item.href"><i class="iconfont" :class="item.icon"></i>{{item.title}}</a></div>-->
-<!--            </div>-->
-<!--            <div class="footer-item">-->
-<!--                <div style="font-size:17px;font-weight: bold;">资源</div>-->
-<!--                <div><a target="_blank" class="out-link" href="https://segmentfault.com/weekly?utm_source=sf-footer">每周精选</a></div>-->
-<!--                <div><a target="_blank" class="out-link" href="https://www.jmjc.tech/">简明教程</a></div>-->
-<!--                <div><a target="_blank" class="out-link" href="https://www.liaoxuefeng.com/">廖雪峰的官方网站</a></div>-->
-<!--            </div>-->
-<!--            <div class="footer-item">-->
-<!--&lt;!&ndash;                <div>本站已苟活 {{runTimeInterval}}</div>&ndash;&gt;-->
-<!--                <div><a target="_blank" class="out-link" href="#">☞后台管理</a></div>-->
-<!--            </div>-->
-<!--        </div>-->
-        <div class="copyright">Copyright © 2020 by <a target="_blank" class="out-link" href="https://www.fengziy.cn">fengziy.cn</a> . All rights reserved. | <a target="_blank" class="out-link" href="http://www.beian.miit.gov.cn">渝ICP备17015355号-1</a></div>
+        <div class="copyright">
+            © 2020  <a target="_blank" class="out-link" href="https://19t.wang">ChunKitAu</a>
+            <br>
+             <a target="_blank" class="out-link" href="#">粤ICP备19161974号-1</a>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "layout-footer",
-        data(){
-            return{
-                socials: []
-            }
-        },
-        components:{
-        },
-        computed:{
-            // runTimeInterval() {
-            //     return this.$store.state.runTimeInterval;
-            // }
-        },
-        methods:{
-            // getSocial(){
-            //     this.$store.dispatch('getSocials').then(data =>{
-            //         this.socials = data
-            //     })
-            // },
-        },
-        created(){
-            // this.getSocial();
-            // this.$store.dispatch('initComputeTime');
-        }
     }
 </script>
 
@@ -57,49 +23,8 @@
     a.out-link:hover{
         color: #ff6d6d;
     }
-    .footer-main{
-        max-width: 800px;
-        margin: 0 auto 20px auto;
-        display: flex;
-        justify-content: space-around;
-        align-items: flex-start;
-        .footer-item{
-            flex: 1;
-            & > div:not(:last-child){
-                margin-bottom: 10px;
-            }
-            i{
-                margin-right: 10px;
-            }
-        }
-    }
     .copyright{
         text-align: center;
     }
 }
-    /*****/
-    @media (max-width: 800px){
-        #layout-footer{
-            .footer-main .footer-item:nth-child(3){
-                flex: 2;
-            }
-        }
-    }
-    @media (max-width: 600px){
-        #layout-footer{
-            .footer-main {
-                display: block;
-                .footer-item{
-                    display: flex;
-                    justify-content: space-around;
-                    align-items: center;
-                    flex-wrap: wrap;
-                    & > div{
-                        margin-bottom: 10px;
-                    }
-                }
-            }
-        }
-    }
-    /*****/
 </style>
